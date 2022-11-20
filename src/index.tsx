6117573e38fa2bcea58ea009c2bb1c74d0d96fb0
Daniel Ong <ongfishh@gmail.com>
2022-11-20 23:51:00 +0800

    enable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
+import React from 'react';
 
 const themes = {
   dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
@@ -10,12 +11,12 @@ const themes = {
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  // <React.StrictMode>
-  <ThemeSwitcherProvider
-    themeMap={themes}
-    defaultTheme={localStorage.getItem('theme') ?? 'light'}
-  >
-    <App />
-  </ThemeSwitcherProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeSwitcherProvider
+      themeMap={themes}
+      defaultTheme={localStorage.getItem('theme') ?? 'light'}
+    >
+      <App />
+    </ThemeSwitcherProvider>
+  </React.StrictMode>
 );
